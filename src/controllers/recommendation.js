8b5d8676ffdb2f6b2e60557c9571239c1fd86e16
anajuliabit <anajuliabit@gmail.com>
2020-09-14 20:43:47 -0300

    feat: rules analyze

--- src/controllers/recommendation.js
+++ src/controllers/recommendation.js
@@ -1,14 +1,87 @@
 const NLU = require("../services/natural-language-understanding");
 
 const classification = {
-  consumo: "DUCATO",
-  seguranca: "MAREA",
-  desempenho: "RENEGADE",
-  manutencao: "FIORINO",
-  conforto: "ARGO",
-  design: "FIAT 500",
-  acessorios: "TORO",
+  CONSUMO: ["DUCATO", "FIORINO"],
+  SEGURANCA: ["MAREA", "DUCATO"],
+  DESEMPENHO: ["RENEGADE", "FIORINO"],
+  MANUTENCAO: ["FIORINO", "ARGO"],
+  CONFORTO: ["ARGO", "RENEGADE"],
+  DESIGN: ["FIAT 500", "TORO"],
+  ACESSORIOS: ["TORO", "FIAT 500"],
 };
+
+const priority = [
+  "SEGURANCA",
+  "CONSUMO",
+  "DESEMPENHO",
+  "MANUTENCAO",
+  "CONFORTO",
+  "DESIGN",
+  "ACESSORIOS",
+];
+
+function diff(num1, num2) {
+  if (num1 > num2) {
+    return num1 - num2;
+  } else {
+    return num2 - num1;
+  }
+}
+
+async function callAnalyze(analyzeParams, car) {
+  const analyze = await NLU.analyze(analyzeParams);
+
+  if (analyze.entities && analyze.entities.length) {
+    const entities = analyze.entities
+      .filter((entitiy) => entitiy.sentiment.label === "negative")
+      .map((entitiy) => {
+        return {
+          entity: entitiy.type,
+          sentiment: entitiy.sentiment.score,
+          mention: entitiy.text,
+        };
+      });
+
+    let sentiment = entities[0].sentiment;
+    let betterScore = entities[0].entity;
+
+    for (let index = 1; index < entities.length; index++) {
+      if (
+        entities[index].sentiment < sentiment &&
+        diff(Math.abs(sentiment), Math.abs(entities[index].sentiment)) > 0.1
+      ) {
+        sentiment = entities[index].sentiment;
+        betterScore = entities[index].entity;
+      } else if (
+        (diff(Math.abs(sentiment), Math.abs(entities[index].sentiment)) < 0.1 ||
+          sentiment === entities[index].sentiment) &&
+        priority.indexOf(betterScore) > priority.indexOf(entities[index].entity)
+      ) {
+        sentiment = entities[index].sentiment;
+        betterScore = entities[index].entity;
+      }
+    }
+
+    if (betterScore) {
+      if (
+        classification[betterScore][0].toUpperCase().trim() !==
+        car.toUpperCase().trim()
+      ) {
+        return {
+          recomendation: classification[betterScore][0],
+          entities,
+        };
+      }
+      return {
+        recomendation: classification[betterScore][1],
+        entities,
+      };
+    }
+    return { recomendation: "", entities: [] };
+  }
+
+  return { recomendation: "", entities: [] };
+}
 
 module.exports = {
   async text(req, res) {
@@ -23,17 +96,7 @@ module.exports = {
         },
       },
     };
-
-    const response = await NLU.analyze(analyzeParams);
-    let sentiment = 0;
-    let classification = "";
-    for (const entitiy of response.entities) {
-      if (entitiy.sentiment.score < sentiment) {
-        sentiment = entitiy.sentiment.score;
-        classification = entitiy.type;
-      }
-    }
-    console.log(response);
-    res.json({ classification });
+    const response = await callAnalyze(analyzeParams, req.body.car);
+    res.json(response);
   },
 };
